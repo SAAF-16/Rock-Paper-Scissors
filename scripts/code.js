17c0a735ca95d482abed95c9b1bd336b319d15ff
SAAF-16 <samsnow16@gmail.com>
2021-03-09 21:32:45 +0100

    removed useless functions

--- scripts/code.js
+++ scripts/code.js
@@ -1,21 +1,7 @@
 function computerPlay(){
     return Math.floor(Math.random()*3);
 }
-function convert(play){
-    switch (true){
-        case (play.toLowerCase()=="rock"):play=0;break;
-        case (play.toLowerCase()=="paper"):play=1;break;
-        case (play.toLowerCase()=="scissors"):play=2;break;
-    }
-    return play;
-}
-function stringPlay(n){
-    switch (true){
-        case n==0:return "rock"
-        case n==1:return "paper"
-        case n==2:return "scissors"
-}
-}
+let choice=["rock","paper","scissors"];
 let nRounds=prompt("how many rounds do you want to play?")
 let userScore=0;
 let pcScore=0;
@@ -25,15 +11,17 @@ for(let i=0;i<nRounds;i++){
     x=true;
     while(x){
     userPlay= prompt("Rock, Paper or Scissors ?");
-    if((userPlay.toLowerCase()=="rock")||(userPlay.toLowerCase()=="paper")||(userPlay.toLowerCase()=="scissors")){
+    if((userPlay.toLowerCase()==choice[0])||(userPlay.toLowerCase()==choice[1])||(userPlay.toLowerCase()==choice[2])){
         x=false;
         break;
     }
     alert("insert a valid input");
     }
-    userPlay=convert(userPlay);
+    userPlay=(userPlay==choice[0])?userPlay=0
+    :(userPlay==choice[1])?userPlay=1
+    :userPlay=2;
     let pcPlay=computerPlay();
-    alert(`Computer played ${ (stringPlay(pcPlay)) } !`);
+    alert(`Computer played ${ choice[pcPlay] } !`);
     if(((userPlay+2)%3)==(pcPlay)){
         alert("you won!")
         userScore++;
