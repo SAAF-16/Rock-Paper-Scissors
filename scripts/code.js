5f3870e3b4fb4d0ed92469e37373689225373f6b
SAAF-16 <samsnow16@gmail.com>
2021-03-10 10:33:46 +0100

    switched ternary operator with if-else

--- scripts/code.js
+++ scripts/code.js
@@ -1,46 +1,53 @@
-function computerPlay(){
-    return Math.floor(Math.random()*3);
+function computerPlay() {
+    return Math.floor(Math.random() * 3);
 }
-let choice=["rock","paper","scissors"];
-let x=false;
+let choice = ["rock", "paper", "scissors"];
+let x = false;
 let nRounds;
-let userScore=0;
-let pcScore=0;
+let userScore = 0;
+let pcScore = 0;
 let userPlay;
+let pcPlay;
 
-while(!x){
-    nRounds=+(prompt("You want to play to the best of ? (max 5)"));
-    if(nRounds<=5){
-        x=true;
-    }else{
+while (!x) {
+    nRounds = +(prompt("You want to play to the best of ? (max 5)"));
+    if (nRounds <= 5) {
+        x = true;
+    } else {
         alert("insert a valid input");
     }
 }
 
-for(let i=0;userScore<nRounds&&pcScore<nRounds;i++){
-    x=true;
-    while(x){
-        userPlay= prompt("Rock, Paper or Scissors ?");
-        if((userPlay.toLowerCase()==choice[0])||(userPlay.toLowerCase()==choice[1])||(userPlay.toLowerCase()==choice[2])){
-            x=false;
-        }else{
+for (let i = 0; userScore < nRounds && pcScore < nRounds; i++) {
+    x = true;
+    while (x) {
+        userPlay = prompt("Rock, Paper or Scissors ?");
+        if ((userPlay.toLowerCase() == choice[0]) || (userPlay.toLowerCase() == choice[1]) || (userPlay.toLowerCase() == choice[2])) {
+            x = false;
+        } else {
             alert("insert a valid input");
         }
     }
-    userPlay=(userPlay==choice[0])?userPlay=0
-        :(userPlay==choice[1])?userPlay=1
-        :userPlay=2;
-    let pcPlay=computerPlay();
-    alert(`Computer played ${ choice[pcPlay] } !`);
-    if(((userPlay+2)%3)==(pcPlay)){
+
+    if (userPlay == choice[0]) {
+        userPlay = 0;
+    } else if (userPlay == choice[1]) {
+        userPlay = 1;
+    } else {
+        userPlay = 2;
+    }
+
+    pcPlay = computerPlay();
+    alert(`Computer played ${choice[pcPlay]} !`);
+    if (((userPlay + 2) % 3) == (pcPlay)) {
         alert("you won!")
         userScore++;
-    }else if(((userPlay+1)%3)==(pcPlay)){
+    } else if (((userPlay + 1) % 3) == (pcPlay)) {
         alert("you lost..")
         pcScore++;
-    }else{
+    } else {
         alert("it's a draw!")
     }
-    alert(`This was Round ${i+1} \n Your Score : ${userScore} Pc Score : ${pcScore}`) ;
+    alert(`This was Round ${i + 1} \n Your Score : ${userScore} Pc Score : ${pcScore}`);
 }
 alert(`Final score:  \n Your Score : ${userScore} Pc Score : ${pcScore}`);
