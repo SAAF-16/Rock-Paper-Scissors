8914e562fbf27c88822d3955c70381ee96c62dca
SAAF-16 <samsnow16@gmail.com>
2021-03-13 18:23:34 +0100

    added images folder solved animation bugs

--- scripts/code.js
+++ scripts/code.js
@@ -2,6 +2,10 @@
     return Math.floor(Math.random() * 3);
 }
 let choice = ["Rock", "Paper", "Scissors"];
+let comments=[["Good play! Rock breaks Scissors","Damn! you got wrapped by the paper","Rock on Rock.. it's a draw"],
+["Ooh yes! that rock didn't had a chance","Well.. it literally cut you off..","Two paper.. we can write that's a draw"],
+["Zac Zac! you cut that paper in pieces ","No chances against that rock","You can't cut but didn't get cut , it's a draw"]
+]
 let x = false;
 let nRounds;
 let userScore = 0;
@@ -9,7 +13,7 @@ let pcScore = 0;
 let userPlay;
 let pcPlay;
 
-/* while (!x) {
+while (!x) {
     nRounds = +(prompt("You want to play to the best of ? (max 5)"));
     if (nRounds <= 5) {
         x = true;
@@ -17,7 +21,7 @@ let pcPlay;
         alert("insert a valid input");
     }
 }
-
+/* 
 for (let i = 0; userScore < nRounds && pcScore < nRounds; i++) {
      x = true;
     while (x) {
@@ -70,6 +74,23 @@ plays.forEach(play => {
 });
 
 
+function checkEnd(comment){
+    if(userScore==nRounds||pcScore==nRounds){
+        if(userScore>pcScore){
+            comment.textContent="Let's Gooo! We made it!!!"
+        }else{
+            comment.textContent=".. you really lost to a random AI"
+        }
+    }
+}
+
+function cleanFloor(){
+    const ufloor= document.querySelector("#userPlay");
+    const pcfloor= document.querySelector("#pcPlay");
+    ufloor.className="";
+    pcfloor.className="";
+
+}
 function userSelectPlay() {
     return uPlayed => {
         if (`${uPlayed.id}`[0] == "p")
@@ -81,12 +102,30 @@ function userSelectPlay() {
             pcPlay = computerPlay();
             
         });
-        uPlayed.addEventListener("click", playAnimation);
-        uPlayed.addEventListener("click", calculateWinner);
+        uPlayed.addEventListener("click",gameTime());
         
     };
 }
+function gameTime() {
+    return (e) => {
+        if(userScore==nRounds||pcScore==nRounds)return //so that after the game is finish nothing is clickable
+        playAnimation(e);
+        calculateWinner(e);
+
+    };
+}
+
+function updateScore(){
+    const uSide= document.querySelector("#userScore");
+    const pSide= document.querySelector("#pcScore");
+    uSide.textContent=userScore;
+    pSide.textContent=pcScore;
+
+}
 function calculateWinner(){
+    const comment=document.querySelector("#pComment")
+    const uSide= document.querySelector("#userPlay");
+    const pSide= document.querySelector("#pcPlay");
     if (userPlay == choice[0]) {
         userPlay = 0;
     } else if (userPlay == choice[1]) {
@@ -95,14 +134,24 @@ function calculateWinner(){
         userPlay = 2;
     }
     if (((userPlay + 2) % 3) == (pcPlay)) {
-        console.log("you won!")
+        console.log("you won!");
+        comment.textContent=comments[userPlay][0];
+        uSide.classList.add("winner");
+        pSide.classList.add("loser");
         userScore++;
+        updateScore()
     } else if (((userPlay + 1) % 3) == (pcPlay)) {
         console.log("you lost..")
+        comment.textContent=comments[userPlay][1];
+        pSide.classList.add("winner");
+        uSide.classList.add("loser");
         pcScore++;
+        updateScore()
     } else {
         console.log("it's a draw!")
+        comment.textContent=comments[userPlay][2];
     }
+    checkEnd(comment);
 }
 function playAnimation(e){
     const uPlay = document.querySelector(`.playable[id=${e.target.id}]`);
@@ -112,4 +161,5 @@ function playAnimation(e){
 }
 function removeAnimation(){
     this.classList.remove("playAnim");
+    cleanFloor();
     }
