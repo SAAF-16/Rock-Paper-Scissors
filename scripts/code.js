a8a4b934a8705b780be8faf55d242c44c03657b1
SAAF-16 <samsnow16@gmail.com>
2021-03-14 11:42:19 +0100

    added media querys to play from phone

--- scripts/code.js
+++ scripts/code.js
@@ -1,6 +1,3 @@
-function computerPlay() {
-    return Math.floor(Math.random() * 3);
-}
 let choice = ["Rock", "Paper", "Scissors"];
 let comments = [["Good play! Rock breaks Scissors", "Damn! you got wrapped by the paper", "Rock on Rock.. it's a draw"],
 ["Ooh yes! that rock didn't had a chance", "Well.. it literally cut you off..", "Two paper.. we can write that's a draw"],
@@ -13,42 +10,7 @@ let pcScore = 0;
 let userPlay;
 let pcPlay;
 let round=0;
-/* ////////////////////// OLD CODE ///////////////////////////////////////////
-for (let i = 0; userScore < nRounds && pcScore < nRounds; i++) {
-     x = true;
-    while (x) {
-        userPlay = prompt("Rock, Paper or Scissors ?");
-        if ((userPlay.toLowerCase() == choice[0]) || (userPlay.toLowerCase() == choice[1]) || (userPlay.toLowerCase() == choice[2])) {
-            x = false;
-        } else {
-            alert("insert a valid input");
-        }
-    } 
-    userplay=
 
-    if (userPlay == choice[0]) {
-        userPlay = 0;
-    } else if (userPlay == choice[1]) {
-        userPlay = 1;
-    } else {
-        userPlay = 2;
-    }
-
-    pcPlay = computerPlay();
-    alert(`Computer played ${choice[pcPlay]} !`);
-    if (((userPlay + 2) % 3) == (pcPlay)) {
-        alert("you won!")
-        userScore++;
-    } else if (((userPlay + 1) % 3) == (pcPlay)) {
-        alert("you lost..")
-        pcScore++;
-    } else {
-        alert("it's a draw!")
-    }
-    alert(`This was Round ${i + 1} \n Your Score : ${userScore} Pc Score : ${pcScore}`);
-}
-alert(`Final score:  \n Your Score : ${userScore} Pc Score : ${pcScore}`);  */
-//////////////////////////// END OLD CODE////////////////////////////////////////////////////////////////
 document.querySelector("body").classList.add("visible");
 setTimeout(function () { begin(); }, 500);
 
@@ -60,17 +22,11 @@ plays.forEach(endOfAnimation());
 
 
 
-function endOfAnimation() {
-    return play => {
-        play.addEventListener("animationend", removeAnimation);
-    };
-}
-
-function begin() {
+function begin() {  //starts the game 
     x = false;
     while (!x) {
-        nRounds = +(prompt("You want to play to the best of ? (max 5)"));
-        if (nRounds <= 5) {
+        nRounds = +(prompt("You want to play to the best of ? (max 10)"));
+        if (nRounds <= 10) {
             x = true;
         } else {
             alert("insert a valid input");
@@ -78,7 +34,89 @@ function begin() {
     }
     updateScore()
 }
+function updateScore() {
+    const userBoard = document.querySelector("#userScore");
+    const pcBoard = document.querySelector("#pcScore");
+    const currRound = document.querySelector("#pRound")
+    userBoard.textContent = userScore;
+    pcBoard.textContent = pcScore;
+    currRound.textContent = round;
 
+}
+function computerPlay() {
+    return randomNumber();
+}
+function randomNumber() {
+    return Math.floor(Math.random() * 3);
+}
+function userSelectPlay() {
+    return uPlayed => {
+
+        if (`${uPlayed.id}`[0] == "p")
+            return; //if a computer tile is pressed it doesn't go on
+
+        uPlayed.addEventListener("click", (e) => {
+            userPlay = `${e.target.id}`.slice(1);
+            pcPlay = computerPlay();
+
+        });
+        uPlayed.addEventListener("click", gameTime());
+
+    };
+}
+function gameTime() {
+    return (e) => {
+        playAnimation(e);
+        calculateWinner(e);
+
+    };
+}
+function playAnimation(e) {
+    const uPlay = document.querySelector(`.playable[id=${e.target.id}]`);
+    uPlay.classList.toggle("playAnim");
+    const pPlay = document.querySelector(`.playable[id=p${choice[pcPlay]}]`);
+    pPlay.classList.toggle("playAnim");
+}
+function endOfAnimation() {
+    return play => {
+        play.addEventListener("animationend", removeAnimation);
+    };
+}
+function removeAnimation() {
+    this.classList.remove("playAnim");
+    cleanFloor();
+}
+function calculateWinner() {
+    const comment = document.querySelector("#pComment")
+    const currRound = document.querySelector("#pRound")
+    const userBoard = document.querySelector("#userPlay");
+    const pcBoard = document.querySelector("#pcPlay");
+    if (userPlay == choice[0]) {
+        userPlay = 0;
+    } else if (userPlay == choice[1]) {
+        userPlay = 1;
+    } else {
+        userPlay = 2;
+    }
+    if (((userPlay + 2) % 3) == (pcPlay)) {
+        comment.textContent = comments[userPlay][0];
+        userBoard.classList.add("winner");
+        pcBoard.classList.add("loser");
+        userScore++;
+        updateScore()
+    } else if (((userPlay + 1) % 3) == (pcPlay)) {
+        comment.textContent = comments[userPlay][1];
+        pcBoard.classList.add("winner");
+        userBoard.classList.add("loser");
+        pcScore++;
+        updateScore()
+    } else {
+        comment.textContent = comments[userPlay][2];
+    }
+    round++;
+    currRound.textContent = round;
+    checkEnd(comment);
+}
 function checkEnd(comment) {
     if (userScore == nRounds || pcScore == nRounds) {
         let restart;
@@ -104,7 +142,6 @@ function checkEnd(comment) {
     }
 
 }
-
 function cleanFloor() {
     const ufloor = document.querySelector("#userPlay");
     const pcfloor = document.querySelector("#pcPlay");
@@ -112,78 +149,9 @@ function cleanFloor() {
     pcfloor.className = "";
 
 }
-function userSelectPlay() {
-    if (userScore == nRounds || pcScore == nRounds) return //so that after the game is finish nothing is clickable
-    return uPlayed => {
 
-        if (`${uPlayed.id}`[0] == "p")
-            return; //if a computer tile is pressed it doesn't go on
 
-        uPlayed.addEventListener("click", (e) => {
-            userPlay = `${e.target.id}`.slice(1);
-            pcPlay = computerPlay();
 
-        });
-        uPlayed.addEventListener("click", gameTime());
 
-    };
-}
-function gameTime() {
-    return (e) => {
-        if (userScore == nRounds || pcScore == nRounds) return //so that after the game is finish nothing is clickable
-        playAnimation(e);
-        calculateWinner(e);
 
-    };
-}
 
-function updateScore() {
-    const uSide = document.querySelector("#userScore");
-    const pSide = document.querySelector("#pcScore");
-    const currRound = document.querySelector("#pRound")
-    uSide.textContent = userScore;
-    pSide.textContent = pcScore;
-    currRound.textContent = round;
-
-}
-function calculateWinner() {
-    const comment = document.querySelector("#pComment")
-    const currRound = document.querySelector("#pRound")
-    const uSide = document.querySelector("#userPlay");
-    const pSide = document.querySelector("#pcPlay");
-    if (userPlay == choice[0]) {
-        userPlay = 0;
-    } else if (userPlay == choice[1]) {
-        userPlay = 1;
-    } else {
-        userPlay = 2;
-    }
-    if (((userPlay + 2) % 3) == (pcPlay)) {
-        comment.textContent = comments[userPlay][0];
-        uSide.classList.add("winner");
-        pSide.classList.add("loser");
-        userScore++;
-        updateScore()
-    } else if (((userPlay + 1) % 3) == (pcPlay)) {
-        comment.textContent = comments[userPlay][1];
-        pSide.classList.add("winner");
-        uSide.classList.add("loser");
-        pcScore++;
-        updateScore()
-    } else {
-        comment.textContent = comments[userPlay][2];
-    }
-    round++;
-    currRound.textContent = round;
-    checkEnd(comment);
-}
-function playAnimation(e) {
-    const uPlay = document.querySelector(`.playable[id=${e.target.id}]`);
-    uPlay.classList.toggle("playAnim");
-    const pPlay = document.querySelector(`.playable[id=p${choice[pcPlay]}]`);
-    pPlay.classList.toggle("playAnim");
-}
-function removeAnimation() {
-    this.classList.remove("playAnim");
-    cleanFloor();
-}
