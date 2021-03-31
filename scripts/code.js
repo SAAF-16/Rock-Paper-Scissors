cf0f541f892ba82e995542d7c104a69a97434dde
SAAF-16 <samsnow16@gmail.com>
2021-03-31 21:35:00 +0200

    implemented popups for game start and game over

--- scripts/code.js
+++ scripts/code.js
@@ -2,7 +2,7 @@ let choice = ["Rock", "Paper", "Scissors"];
 let comments = [["Good play! Rock breaks Scissors", "Damn! you got wrapped by the paper", "Rock on Rock.. it's a draw"],
 ["Ooh yes! that rock didn't had a chance", "Well.. it literally cut you off..", "Two paper.. we can write that's a draw"],
 ["Zac Zac! you cut that paper in pieces ", "No chances against that rock", "You can't cut but didn't get cut , it's a draw"]
-]
+];
 let x = false;
 let nRounds;
 let userScore = 0;
@@ -11,8 +11,19 @@ let userPlay;
 let pcPlay;
 let round = 0;
 
-document.querySelector("body").classList.add("visible");
-setTimeout(function () { begin(); }, 500);
+const comment = document.querySelector("#pComment");
+const currRound = document.querySelector("#pRound");
+const userBoard = document.querySelector("#userPlay");
+const pcBoard = document.querySelector("#pcPlay");
+const gameContainer = document.getElementById("gameContainer");
+
+const gameStart = document.getElementById('game-start');
+gameStart.addEventListener('click', showGameStart());
+
+const gameOver = document.getElementById('game-over');
+gameOver.addEventListener('click', showGameOver());
+
+document.querySelector("selectors");
 
 const playable = document.querySelectorAll(".playable");
 playable.forEach(userSelectPlay());
@@ -22,22 +33,37 @@ plays.forEach(endOfAnimation());
 
 
 
-function begin() {  //starts the game 
-    x = false;
-    while (!x) {
-        nRounds = +(prompt("You want to play to the best of ? (max 10)"));
-        if (nRounds <= 10) {
-            x = true;
-        } else {
-            alert("insert a valid input");
-        }
-    }
-    updateScore()
+updateScore();
+
+
+
+function showGameOver() {
+    return (e) => {
+        if (e.target.nodeName !== 'BUTTON')
+            return false;
+        gameOver.classList.remove('movein');
+        gameOver.classList.add('moveout');
+        gameStart.classList.remove('moveout');
+        gameStart.classList.add('movein');
+        comment.textContent = "So we back at it again!";
+    };
 }
+
+function showGameStart() {
+    return (e) => {
+        if (e.target.nodeName !== 'BUTTON')
+            return false;
+        nRounds = e.target.textContent;
+        gameStart.classList.remove('movein');
+        gameStart.classList.add('moveout');
+        gameContainer.style.opacity = 1;
+    };
+}
+
 function updateScore() {
     const userBoard = document.querySelector("#userScore");
     const pcBoard = document.querySelector("#pcScore");
-    const currRound = document.querySelector("#pRound")
+    const currRound = document.querySelector("#pRound");
     userBoard.textContent = userScore;
     pcBoard.textContent = pcScore;
     currRound.textContent = round;
@@ -51,7 +77,7 @@ function randomNumber() {
 }
 function userSelectPlay() {
     return uPlayed => {
-        if(userScore==nRounds||pcScore==nRounds)return;//so that nothing is clickable
+        if (userScore == nRounds || pcScore == nRounds) return;//so that nothing is clickable
         if (`${uPlayed.id}`[0] == "p")
             return; //if a computer tile is pressed it doesn't go on
 
@@ -66,7 +92,7 @@ function userSelectPlay() {
 }
 function gameTime() {
     return (e) => {
-        if(userScore==nRounds||pcScore==nRounds)return;//so that nothing is clickable
+        if (userScore == nRounds || pcScore == nRounds) return;//so that nothing is clickable
         playAnimation(e);
         calculateWinner(e);
 
@@ -88,10 +114,7 @@ function removeAnimation() {
     cleanFloor();
 }
 function calculateWinner() {
-    const comment = document.querySelector("#pComment")
-    const currRound = document.querySelector("#pRound")
-    const userBoard = document.querySelector("#userPlay");
-    const pcBoard = document.querySelector("#pcPlay");
+
     if (userPlay == choice[0]) {
         userPlay = 0;
     } else if (userPlay == choice[1]) {
@@ -104,42 +127,32 @@ function calculateWinner() {
         userBoard.classList.add("winner");
         pcBoard.classList.add("loser");
         userScore++;
-        updateScore()
+        updateScore();
     } else if (((userPlay + 1) % 3) == (pcPlay)) {
         comment.textContent = comments[userPlay][1];
         pcBoard.classList.add("winner");
         userBoard.classList.add("loser");
         pcScore++;
-        updateScore()
+        updateScore();
     } else {
         comment.textContent = comments[userPlay][2];
     }
     round++;
     currRound.textContent = round;
-    checkEnd(comment);
+    checkEnd();
 }
-function checkEnd(comment) {
+function checkEnd() {
     if (userScore == nRounds || pcScore == nRounds) {
-        let restart;
         if (userScore > pcScore) {
-            comment.textContent = "Let's Gooo! We made it!!!"
+            comment.textContent = "Let's Gooo! We made it!!!";
         } else {
-            comment.textContent = ".. you really lost to a random AI"
+            comment.textContent = ".. you really lost to a random AI";
         }
-        setTimeout(function () {
-            restart = prompt("Do you want to play again? (yes or no)");
-            if (restart.toLowerCase() == "yes") {
-                userScore = 0;
-                pcScore = 0;
-                round = 0;
-                updateScore();
-                comment.textContent = "So we back at it again!"
-                begin();
-
-            }
-        }, 1500);
-
-
+        userScore = 0;
+        pcScore = 0;
+        round = 0;
+        updateScore();
+        gameOver.classList.add('movein');
     }
 
 }
